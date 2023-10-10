cd7fdfddf430d53fcc14ec45db50a927b02f1248
Gaurav Gupta <65570469+Gaurav2119@users.noreply.github.com>
2023-10-10 13:13:40 +0530

    Assignment 2

---
+++ Assignment_2/Exercise_4/Exercise_4/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    public enum EquipmentType
+    {
+        Mobile,
+        Immobile
+    }
+    public abstract class Equipment
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double DistanceMovedTillDate { get; protected set; } = 0;
+        public double MaintenanceCost { get; protected set; } = 0;
+        public abstract EquipmentType Type { get; }
+        public virtual void MoveBy(double distance)
+        {
+            DistanceMovedTillDate += distance;
+            MaintenanceCost += GetMaintenanceCost(distance);
+        }
+
+        protected abstract double GetMaintenanceCost(double distance);
+    }
+    public class MobileEquipment : Equipment
+    {
+        public int NumberOfWheels { get; set; }
+        public override EquipmentType Type => EquipmentType.Mobile;
+        protected override double GetMaintenanceCost(double distance)
+        {
+            return NumberOfWheels * distance;
+        }
+    }
+    public class ImmobileEquipment : Equipment
+    {
+        public double Weight { get; set; }
+        public override EquipmentType Type => EquipmentType.Immobile;
+        protected override double GetMaintenanceCost(double distance)
+        {
+            return Weight * distance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mobileEquipment = new MobileEquipment
+            {
+                Name = "JCB",
+                Description = "Heavy Equipment",
+                NumberOfWheels = 4
+            };
+
+            var immobileEquipment = new ImmobileEquipment
+            {
+                Name = "Ladder",
+                Description = "Portable Steps",
+                Weight = 5
+            };
+
+            mobileEquipment.MoveBy(10);
+            immobileEquipment.MoveBy(5);
+
+            Console.WriteLine($"Mobile Equipment Name: {mobileEquipment.Name}");
+            Console.WriteLine($"Mobile Equipment Type: {mobileEquipment.Type}");
+            Console.WriteLine($"{mobileEquipment.Name} Wheels: {mobileEquipment.NumberOfWheels}");
+            Console.WriteLine($"Mobile Equipment Distance Moved: {mobileEquipment.DistanceMovedTillDate}");
+            Console.WriteLine($"Mobile Equipment Maintenance Cost: {mobileEquipment.MaintenanceCost}");
+
+            Console.WriteLine($"\n\nImmobile Equipment Name: {immobileEquipment.Name}");
+            Console.WriteLine($"Immobile Equipment Type: {immobileEquipment.Type}");
+            Console.WriteLine($"{immobileEquipment.Name} Wheels: {immobileEquipment.Weight}");
+            Console.WriteLine($"Immobile Equipment Distance Moved: {immobileEquipment.DistanceMovedTillDate}");
+            Console.WriteLine($"Immobile Equipment Maintenance Cost: {immobileEquipment.MaintenanceCost}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment_2/Exercise_5/Exercise_5/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        public enum DuckType
+        {
+            Rubber,
+            Mallard,
+            Redhead
+        }
+
+        public interface IFlyable
+        {
+            void Fly();
+        }
+
+        public interface IQuackable
+        {
+            void Quack();
+        }
+
+        public abstract class Duck
+        {
+            public double Weight { get; set; }
+            public int NumWings { get; set; }
+            public DuckType Type { get; set; }
+
+            public Duck(double weight, int numWings, DuckType type)
+            {
+                Weight = weight;
+                NumWings = numWings;
+                Type = type;
+            }
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Weight: {Weight}");
+                Console.WriteLine($"Number of Wings: {NumWings}");
+                Console.WriteLine($"Type: {Type}");
+                Fly();
+                Quack();
+            }
+
+            public abstract void Fly();
+            public abstract void Quack();
+        }
+
+        public class RubberDuck : Duck, IQuackable
+        {
+            public RubberDuck(double weight, int numWings) : base(weight, numWings, DuckType.Rubber) { }
+            public override void Quack()
+            {
+                Console.WriteLine("Quack: Squeak!");
+            }
+            public override void Fly()
+            {
+                Console.WriteLine("Rubber ducks don't fly.");
+            }
+        }
+
+        public class MallardDuck : Duck, IFlyable, IQuackable
+        {
+            public MallardDuck(double weight, int numWings) : base(weight, numWings, DuckType.Mallard) { }
+            public override void Fly()
+            {
+                Console.WriteLine("Flying fast.");
+            }
+            public override void Quack()
+            {
+                Console.WriteLine("Quack: Loud!");
+            }
+        }
+
+        public class RedheadDuck : Duck, IFlyable, IQuackable
+        {
+            public RedheadDuck(double weight, int numWings) : base(weight, numWings, DuckType.Redhead) { }
+            public override void Fly()
+            {
+                Console.WriteLine("Flying slow.");
+            }
+            public override void Quack()
+            {
+                Console.WriteLine("Quack: Mild!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Duck rubberDuck = new RubberDuck(0.1, 0);
+            rubberDuck.ShowDetails();
+
+            Duck mallardDuck = new MallardDuck(1.2, 2);
+            mallardDuck.ShowDetails();
+
+            Duck redheadDuck = new RedheadDuck(0.8, 2);
+            redheadDuck.ShowDetails();
+            
+            Console.ReadLine();
+        }
+    }
+}
