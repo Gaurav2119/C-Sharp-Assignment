29ededd01519e713087a8c8abe3b96c5208fc74b
Gaurav Gupta <65570469+Gaurav2119@users.noreply.github.com>
2023-10-10 13:21:58 +0530

    Assignment 7

---
+++ Assignment_7/Exercise_16/Exercise_16/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the path of the directory to search: ");
+            string dirPath = Console.ReadLine();
+
+            // Check directory is present
+            if (Directory.Exists(dirPath))
+            {
+                string[] files = Directory.GetFiles(dirPath); // return names of files including path
+
+                // Check files are present
+                if (files.Length != 0)
+                {
+                    // 1. Number of text files in the directory
+                    int totalTextFiles = files.Count(f => Path.GetExtension(f) == ".txt");
+                    Console.WriteLine("\nNumber of text files in the directory: {0}", totalTextFiles);
+
+                    // 2. Number of files per extension type
+                    var filesByExt = files.GroupBy(f => Path.GetExtension(f)).Select(g => new { Extension = g.Key, Count = g.Count() });
+                    Console.WriteLine("\nFiles by extension:");
+                    foreach (var file in filesByExt) Console.WriteLine(" {0}: {1}", file.Extension, file.Count);
+
+                    // 3. Top 5 largest files with their file size
+                    var top5Files = files.OrderByDescending(f => new FileInfo(f).Length).Take(5).Select(f => new { Name = Path.GetFileName(f), Size = new FileInfo(f).Length });
+                    Console.WriteLine("\nTop 5 largest files with their size:");
+                    foreach (var file in top5Files) Console.WriteLine(" {0} having size {1} bytes", file.Name, file.Size);
+
+                    // 4. File with maximum length
+                    var maxLengthFile = files.OrderByDescending(f => new FileInfo(f).Length).First();
+                    Console.WriteLine("\nFile with maximum size: \n {0} having size {1} bytes", Path.GetFileName(maxLengthFile), new FileInfo(maxLengthFile).Length);
+                }
+
+                else
+                {
+                    Console.WriteLine("No file is present in the given directory.");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("The directory does not exist.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment_7/Exercise_17/Exercise_17/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_17
+{
+    class CustomException : Exception
+    {
+        public CustomException(string message) : base(message) { Console.WriteLine("Try Again"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timesPlayed = 0;
+
+            while (timesPlayed < 5)
+            {
+                Console.Write("Enter any number from 1 - 5: ");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int userInput);
+                try
+                {
+                    if (userInput < 1 || userInput > 5) throw new CustomException("Invalid input! Enter a number from 1 - 5");
+                }
+                catch (CustomException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                Console.WriteLine();
+                switch (userInput)
+                {
+                    case 1: 
+                        Console.Write("Enter even number: ");
+                        break;
+                    case 2:
+                        Console.Write("Enter odd number: ");
+                        break;
+                    case 3:
+                        Console.Write("Enter prime number: ");
+                        break;
+                    case 4:
+                        Console.Write("Enter a negative number: ");
+                        break;
+                    case 5:
+                        Console.Write("Enter zero: ");
+                        break;
+                    default:
+                        Console.Write("Invalid input! Enter a number from 1 - 5");
+                        continue;
+                }
+                input = Console.ReadLine();
+                int.TryParse(input, out int number);
+
+                try
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            if (number % 2 == 0) Console.WriteLine("Success");
+                            else throw new CustomException("Error! Enter an even number\n");
+                            break;
+                        case 2:
+                            if (number % 2 != 0) Console.WriteLine("Success");
+                            else throw new CustomException("Error! Enter an even number\n");
+                            break;
+                        case 3:
+                            bool isPrime = true;
+                            if (number < 2) isPrime = false;
+                            for (int i = 2; i < Math.Sqrt(number); i++)
+                            {
+                                if (number % i == 0)
+                                {
+                                    isPrime = false;
+                                    break;
+                                }
+                            }
+                            if (isPrime) Console.WriteLine("Success");
+                            else throw new CustomException("Error! Enter an prime number\n");
+                            break;
+                        case 4:
+                            if (number < 0) Console.WriteLine("Success");
+                            else throw new CustomException("Error! Enter an negative number\n");
+                            break;
+                        case 5:
+                            if (number == 0) Console.WriteLine("Success");
+                            else throw new CustomException("Error! Enter zero\n");
+                            break;
+                    }
+                }
+                catch (CustomException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                timesPlayed++;
+
+                if (timesPlayed == 5)
+                {
+                    Console.WriteLine("\nYou have played this game for 5 times.\nCome Back Again.");
+                    break;
+                }
+                Console.WriteLine("Play Again.\n");
+            } 
+            Console.ReadLine();
+        }
+    }
+}
