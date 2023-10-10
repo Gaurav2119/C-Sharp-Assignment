2b0b02c44843b030d53242d78928bc59b8b10fe7
Gaurav Gupta <65570469+Gaurav2119@users.noreply.github.com>
2023-10-10 13:17:09 +0530

    Assignment 4

---
+++ Assignment_4/Exercise_8/Exercise_8/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<string> priorityQueue = new PriorityQueue<string>();
+            priorityQueue.Enqueue(3, "Iooawi");
+            priorityQueue.Enqueue(1, "Zuika");
+            priorityQueue.Enqueue(2, "Aki");
+            priorityQueue.Enqueue(5, "Iak");
+
+            Console.WriteLine($"Top item: -> {priorityQueue.Peek()}");
+            Console.WriteLine($"Removed item: -> {priorityQueue.Dequeue()}");
+            Console.WriteLine($"Top item: -> {priorityQueue.Peek()}");
+            Console.WriteLine($"Items present in this queue {priorityQueue.Count()}");
+            Console.WriteLine($" ---> {priorityQueue.Contains("Iak")}");
+            Console.WriteLine($"{priorityQueue.getHighestPriority()}");
+
+            Console.ReadLine();
+        }
+    }
+
+    class PriorityQueue<T> where T : IEquatable<T>
+    {
+        private IDictionary<int, IList<T>> elements;
+        public PriorityQueue()
+        {
+            elements = new SortedDictionary<int, IList<T>>();
+        }
+        public PriorityQueue(IDictionary<int, IList<T>> elements) : this()
+        {
+        }
+        public int Count()
+        {
+            return elements.Count;
+        }
+        public bool Contains(T item)
+        {
+            bool res = false;
+            foreach (KeyValuePair<int, IList<T>> pair in elements)
+            {
+                if (pair.Value[0].Equals(item))
+                {
+                    Console.Write($"{item} Found ---> {pair.Key} {pair.Value[0]}");
+                    res = true;
+                }
+            }
+            return res;
+        }
+        public T Dequeue()
+        {
+            IList<T> list = elements[elements.Keys.First()];
+            int priority = elements.Keys.First();
+            T highestPriority = list.First();
+            list.Remove(highestPriority);
+            if(list.Count == 0)
+            {
+                elements.Remove(priority);
+            }
+            return highestPriority;
+        }
+        public void Enqueue(int priority, T item)
+        {
+            IList<T> items;
+            if (!elements.ContainsKey(priority))
+                elements.Add(priority, new List<T>());
+            items = elements[priority];
+            items.Add(item);
+        }
+        public T Peek()
+        {
+            IList<T> priorityList = elements[elements.Keys.First()];
+            return priorityList[0];
+        }
+        public int getHighestPriority()
+        {
+            int Firstkey = elements.Take(1).Select(d => d.Key).First();
+            return Firstkey;
+        }
+    }
+}
---
+++ Assignment_4/Exercise_9/Exercise_9/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    interface IPriority
+    {
+        int Priority { get; set; }
+    }
+
+    class PriorityQueue2<T> : IPriority where T : IEquatable<T>
+    {
+
+        private IDictionary<int, IList<T>> elements;
+        public PriorityQueue2()
+        {
+            elements = new Dictionary<int, IList<T>>();
+        }
+        public PriorityQueue2(IDictionary<int, IList<T>> elements) : this()
+        {
+            this.elements = elements;
+        }
+        public int Count
+        {
+            get
+            {
+
+                return elements.Count;
+            }
+        }
+        public int Priority { get; set; }
+        public bool Contains(T items)
+        {
+            foreach (var list in elements)
+            {
+                foreach (T ele in list.Value)
+                {
+                    if (ele.Equals(items))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public T Dequeue()
+        {
+            if (elements.Count > 0)
+            {
+                int highestPriority = HighestPriority;
+                T ele = elements[highestPriority][0];
+                elements[highestPriority].Remove(ele);
+                if (elements[highestPriority].Count == 0)
+                {
+                    elements.Remove(highestPriority);
+                }
+                return ele;
+            }
+            else
+                throw new Exception("No element found");
+        }
+        public void Enqueue(T item)
+        {
+            if (!elements.ContainsKey(Priority))
+            {
+                elements[Priority] = new List<T>();
+            }
+            elements[Priority].Add(item);
+
+        }
+        public T Peek()
+        {
+            if (elements.Count > 0)
+            {
+                return elements[HighestPriority][0];
+            }
+            else
+                throw new Exception("No element found");
+        }
+        public int HighestPriority
+        {
+            get
+            {
+                List<int> keys = elements.Keys.ToList();
+                keys = keys.OrderByDescending(ele => ele).ToList();
+                return keys[0];
+            }
+        }
+        public int GetHighestPriority()
+        {
+            return this.HighestPriority;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue2<int> pq = new PriorityQueue2<int>();
+
+            //Enque Operation
+            //setting priority with Property
+            pq.Priority = 5;
+            pq.Enqueue(10);
+            pq.Enqueue(30);
+            pq.Priority = 1;
+            pq.Enqueue(90);
+
+            pq.Priority = 10;
+            pq.Enqueue(60);
+            pq.Enqueue(88);
+            pq.Priority = 6;
+            pq.Enqueue(72);
+
+            //peek()
+            Console.WriteLine(pq.Peek());
+            //contain
+            Console.WriteLine(pq.Contains(10));
+            //Deque
+            Console.WriteLine(pq.Dequeue());
+            //count
+            Console.WriteLine(pq.Count);
+            //Get Highest Priority
+            Console.WriteLine(pq.GetHighestPriority());
+
+            Console.ReadLine();
+        }
+    }
+}
